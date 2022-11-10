f43f0a4c55db234fdd86d4604905faf8eb7f33a0
bayesball <albertcb1@gmail.com>
2022-11-10 07:47:11 -0500

    add files

--- November 10/November 10.R
+++ November 10/November 10.R
@@ -1,68 +1,75 @@
-## ----setup, include=FALSE--------------------------------
-knitr::opts_chunk$set(echo = TRUE)
+# Data frame manipulations
 
+# Reference:  Chapter 5 in R for Data Science
+# https://r4ds.had.co.nz/transform.html
 
-## --------------------------------------------------------
+# In the `dplyr` package, here are some verbs
+# for data manipulation:
+
+# - `select` will choose variables (columns)
+# - `filter` will choose observations (rows)
+# - `mutate` will define new variables
+# - `arrange` will sort the data frame with respect to some
+#    variable
+# - `summarize` will compute a data summary
+
+# The piping symbol `%>%` passes the data frame from one operation to another operation.
+
+# A common thing to do is to divide the data frame by a `group_by` on some variable and `summarize` each subgroup.
+
+#  Exploring Lahman's Pitching Table
+
+# Suppose we are interested in finding the pitchers with the highest strikeout rates (9 x SO / IP) in the seasons 1980 through 2021.  Interested in
+
+# the highest rates for a particular season
+# the highest career rates
+
+# Load the `Lahman` and `dplyr` packages.
+
 library(Lahman)
 library(dplyr)
 
-
-## --------------------------------------------------------
-Pitching %>% 
-  filter(yearID >= 1980, yearID <= 2021) %>% 
+Pitching %>%
+  filter(yearID >= 1980, yearID <= 2021) %>%
   group_by(playerID, yearID) %>%
   summarize(SO = sum(SO),
             IP = sum(IPouts / 3)) -> S
 head(S)
 
-
-## --------------------------------------------------------
-S %>% 
+S %>%
   filter(IP >= 100) -> S100
 
-
-## --------------------------------------------------------
-S100 %>% 
-  mutate(SO_Rate = 9 * SO / IP) %>% 
+S100 %>%
+  mutate(SO_Rate = 9 * SO / IP) %>%
   arrange(desc(SO_Rate)) -> S100
 head(S100)
 
-
-## --------------------------------------------------------
 inner_join(select(People, playerID,
                   nameFirst, nameLast),
-           S100, by = "playerID") %>% 
-  mutate(Name = paste(nameFirst, nameLast)) %>% 
-  select(Name, yearID, SO_Rate)  %>% 
+           S100, by = "playerID") %>%
+  mutate(Name = paste(nameFirst, nameLast)) %>%
+  select(Name, yearID, SO_Rate)  %>%
   arrange(desc(SO_Rate)) -> S100
 head(S100)
 
-
-## --------------------------------------------------------
 tail(S100)
 
-
-## --------------------------------------------------------
-Pitching %>% 
-  filter(yearID >= 1980, yearID <= 2021) %>% 
+Pitching %>%
+  filter(yearID >= 1980, yearID <= 2021) %>%
   group_by(playerID) %>%
   summarize(SO = sum(SO),
-            IP = sum(IPouts / 3)) %>% 
-  filter(IP >= 1000) %>% 
-  mutate(SO_Rate = 9 * SO / IP) -> S_cum 
+            IP = sum(IPouts / 3)) %>%
+  filter(IP >= 1000) %>%
+  mutate(SO_Rate = 9 * SO / IP) -> S_cum
 
-
-## --------------------------------------------------------
-  inner_join(select(People, playerID,
+inner_join(select(People, playerID,
                   nameFirst, nameLast),
              S_cum,
-             by = "playerID") -> S_cum 
+             by = "playerID") -> S_cum
 
-
-## --------------------------------------------------------
-S_cum %>% 
-  mutate(Name = paste(nameFirst, nameLast)) %>% 
-  select(Name, IP, SO_Rate) %>% 
-  arrange(desc(SO_Rate)) %>% 
+S_cum %>%
+  mutate(Name = paste(nameFirst, nameLast)) %>%
+  select(Name, IP, SO_Rate) %>%
+  arrange(desc(SO_Rate)) %>%
   head(6)
 
---
+++ November 10/November_10_HW.R
@@ -0,0 +1,27 @@
+# November 10 Assignment
+
+# Pitching is a data frame with 48,399 observations and 30 variables.  A row consists of stats for a specific pitcher for a given season and stint (if a player is traded midseason, he may have several stints within a single season).
+
+# 1.  Assuming at least 100 innings pitched, who had the best season ERA in all seasons 1960 or later?
+
+#  2.  Again assuming at least 100 innings pitched and seasons 1960 or later, who had the best season WHIP value?
+
+#  3.  Considering all seasons 1960 or later, which pitcher had the best career WHIP measure?
+
+#  4.  Suppose you are interested in a pitchers ratio of strikeouts to walks.  For the 2000 season, which pitcher had the best SO / BB ratio?  (Assume at least 100 innings pitched.)
+
+# Teams is a data frame with 2985 observations and 48 variables.  A row consists of season records for a given team in a given season.
+
+#  5.  Which team had the best win/loss percentage in a single season?
+
+#  6.  Find the 10 teams who had the best win/loss records in a single season since 1920.  How many of these teams won the World Series?
+
+#  7.  One can compute the expected number of wins for a team by use of the Bill James formula:
+
+#  E = G \times \frac{R^2}{R^2 + RA^2}
+
+# One can measure a team's success by seeing how many wins a team has above expected.
+
+# Luck = W - E
+
+# For all teams in seasons 1960 or later, which teams had the highest Luck values?  Which teams had the lowest Luck values?
